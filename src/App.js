045ae394607a455e60ea452bfddf462274e3f865
pmNiko <nikolas090189@gmail.com>
2020-10-21 20:11:06 -0300

    Add chart.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Grafico from './components/Grafico';
 
 function App() {
   return (
-    <h1>welcome</h1>
+    <Grafico/>
   );
 }
 
